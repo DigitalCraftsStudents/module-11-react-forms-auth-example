161f3b5324584a906f2b646d93110723b7bd2350
Lachlan Heywood <lachlan@heywood.net.au>
2021-03-24 12:04:23 -0400

    setup auth service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AuthService from "./lib/AuthService";
 
 function App() {
+  checkCredentials(username, password) {
+    AuthService.login(username, password)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Login form will go here */}
     </div>
   );
 }
---
+++ src/lib/AuthService.js
@@ -0,0 +1,60 @@
+/**
+ * Fake Auth Service
+ */
+export default class AuthService {
+  credentials = {
+    huxley: {
+      password: 's3kr3t'
+    }
+  }
+
+  /** currently logged in user */
+  user = null;
+
+  /**
+   * Login to fake auth service
+   * @param {string} username
+   * @param {string} password
+   * @returns login status
+   */
+  login(username, password) {
+    console.log('checking credentials');
+    const userObj = this.credentials[username];
+    if (userObj && (userObj.password === password)) {
+        // If we found a user in this.credentials 
+        // and the passwords match.
+        this.user = userObj;
+        return {
+            isValid: true,
+            message: 'Login successful'
+        };
+    } else {
+        // We couldn't find a user with that username
+        // or the passwords didn't match.
+        return {
+            isValid: false,
+            message: 'Bad username or password'
+        };
+    }
+  }
+
+  /**
+   * 
+   * @returns logged out status
+   */
+  logout() {
+    this.user = null;
+
+    return {
+      message: 'Logged out successfully'
+    };
+  }
+
+  /**
+   * Get the current User
+   * @returns the currently logged in user or null
+   */
+  getUser() {
+    return this.user || null;
+  }
+}
