8cdc8376fe2d364d11d14caf5cb282011699eec8
Lachlan Heywood <lachlan@heywood.net.au>
2021-03-24 13:03:36 -0400

    update with solution part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import './App.css';
+import LoginForm from './components/LoginForm';
 import AuthService from "./lib/AuthService";
 
 class App extends React.Component {
@@ -11,7 +12,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {/* Login form will go here */}
+        <LoginForm />
       </div>
     );
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+export default class LoginForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = (event) => {
+    // const name = event.target.name;
+    // const value = event.target.value;
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              onChange={this.handleChange}
+              value={this.state.username}
+            />
+          </p>
+          <p>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+          </p>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  }
+}
